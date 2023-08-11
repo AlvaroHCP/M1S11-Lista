6dd8210ffe74e73d0c9709881086dcea3581a0f1
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-11 17:59:08 -0300

    Defining some topics to write.

--- m1s11-mini-projeto/src/Pages/Home/index.jsx
+++ m1s11-mini-projeto/src/Pages/Home/index.jsx
@@ -1,12 +1,22 @@
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import { ButtonImageRoutes } from '../../components/Button/ButtonImageRoutes'
+import { HomeStyled } from './styled'
 
 function Home() {
     return (
         <>
             <Header />
-            <ButtonImageRoutes />
+
+            <HomeStyled>
+                {/* <ButtonImageRoutes /> */}
+
+                <h2>Até aqui!</h2>
+                <h2>O que aprendi?</h2>
+                <h2>Conquistas!</h2>
+
+            </HomeStyled>
+
             <Footer />
         </>
     )
---
+++ m1s11-mini-projeto/src/Pages/Home/styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const HomeStyled = styled.main`
+height: 75vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+
+export { HomeStyled }
