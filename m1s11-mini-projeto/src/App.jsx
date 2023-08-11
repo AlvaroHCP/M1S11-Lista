07d51867d81effbe2ecb6d17ecb701e701310976
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-11 16:58:08 -0300

    Creation of the button ButtonImageRoutes

--- m1s11-mini-projeto/src/App.jsx
+++ m1s11-mini-projeto/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import './App.css'
+import { ButtonImageRoutes } from './components/Button/ButtonImageRoutes'
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <ButtonImageRoutes />
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
