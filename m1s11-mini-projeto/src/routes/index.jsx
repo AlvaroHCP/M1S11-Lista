175e4dfac2c84ce2da2e6fd54b6f753f3e4900aa
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-09-16 20:04:04 -0300

    Criando rota para a página AboutMe.

--- m1s11-mini-projeto/src/routes/index.jsx
+++ m1s11-mini-projeto/src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from '../Pages/Home'
 import { NotFound404 } from '../Pages/404'
+import { AboutMe } from "../Pages/AboutMe";
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path={'/'} element={<Home />} />
+            <Route path={'/AboutMe/'} element={<AboutMe />} />
             <Route path={'*'} element={<NotFound404 />} />
         </Routes>
     )
