0c977922fb6607cb6cdc3b139381790852f4c58f
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-09-16 20:44:50 -0300

    Texto da página Portfólio.

--- m1s11-mini-projeto/src/routes/index.jsx
+++ m1s11-mini-projeto/src/routes/index.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from '../Pages/Home'
 import { NotFound404 } from '../Pages/404'
 import { AboutMe } from "../Pages/AboutMe";
+import { Portfolio } from "../Pages/Portfolio";
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path={'/'} element={<Home />} />
             <Route path={'/sobre'} element={<AboutMe />} />
+            <Route path={'/projetos'} element={<Portfolio />} />
             <Route path={'*'} element={<NotFound404 />} />
         </Routes>
     )
